183b92bf9560fade257435bf228cab4364419968
ValdikSS <iam@valdikss.org.ru>
2014-10-31 00:50:48 +0300

    Add Mac OS support

--- skype-poll-fix.c
+++ skype-poll-fix.c
@@ -2,19 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dlfcn.h>
-#include <poll.h>
 
+#ifdef linux
+#include <poll.h>
+#endif
+
+#ifdef __APPLE__
+#include <sys/event.h>
+#include <sys/time.h>
+#endif
 
 char* SET_POLL_C;
 char* MIN_POLL_C;
 
-unsigned int MIN_POLL, SET_POLL = 300;
-
-static int (*poll_orig)(struct pollfd *fds, nfds_t nfds, int timeout);
-
+#ifdef linux
+int MIN_POLL, SET_POLL = 300;
+static int (*pollmethod_orig)(struct pollfd *fds, nfds_t nfds, int timeout);
 int poll(struct pollfd *fds, nfds_t nfds, int timeout) {
-	if (poll_orig == NULL) {
-		poll_orig = dlsym(RTLD_NEXT, "poll");
+#endif
+
+#ifdef __APPLE__
+int MIN_POLL, SET_POLL = 300 * 1000000;
+static int (*pollmethod_orig)(int kq,  const struct kevent *changelist, int nchanges,
+		struct kevent *eventlist, int nevents, const struct timespec *timeout);
+int kevent(int kq,  const struct kevent *changelist, int nchanges, 
+		struct kevent *eventlist, int nevents, const struct timespec *timeout) {
+#endif
+
+	if (pollmethod_orig == NULL) {
+#ifdef linux
+		pollmethod_orig = dlsym(RTLD_NEXT, "poll");
+#endif
+#ifdef __APPLE__
+		pollmethod_orig = dlsym(RTLD_NEXT, "kevent");
+#endif
 		MIN_POLL_C = getenv("MIN_POLL");
 		SET_POLL_C = getenv("SET_POLL");
 		if (MIN_POLL_C) {
@@ -24,8 +45,17 @@ int poll(struct pollfd *fds, nfds_t nfds, int timeout) {
 			SET_POLL = atoi(SET_POLL_C);
 		}
 	}
+#ifdef linux
 	if (timeout < MIN_POLL) {
 		timeout = SET_POLL;
 	}
-	return poll_orig(fds, nfds, timeout);
+	return pollmethod_orig(fds, nfds, timeout);
+#endif
+#ifdef __APPLE__
+	struct timespec new_timeout;
+	new_timeout.tv_nsec = SET_POLL * 1000000;
+	if (timeout->tv_nsec < MIN_POLL * 1000000) {
+		return pollmethod_orig(kq, changelist, nchanges, eventlist, nevents, &new_timeout);
+	}
+#endif
 }
