5383fdd97fb33492d1edd36fd9b8329a2c8cd039
ValdikSS <iam@valdikss.org.ru>
2014-10-31 00:13:08 +0300

    Spaces replaced with tabulation

--- skype-poll-fix.c
+++ skype-poll-fix.c
@@ -4,6 +4,7 @@
 #include <dlfcn.h>
 #include <poll.h>
 
+
 char* SET_POLL_C;
 char* MIN_POLL_C;
 
@@ -12,8 +13,8 @@ unsigned int MIN_POLL, SET_POLL = 300;
 static int (*poll_orig)(struct pollfd *fds, nfds_t nfds, int timeout);
 
 int poll(struct pollfd *fds, nfds_t nfds, int timeout) {
-    if (poll_orig == NULL) {
-        poll_orig = dlsym(RTLD_NEXT, "poll");
+	if (poll_orig == NULL) {
+		poll_orig = dlsym(RTLD_NEXT, "poll");
 		MIN_POLL_C = getenv("MIN_POLL");
 		SET_POLL_C = getenv("SET_POLL");
 		if (MIN_POLL_C) {
@@ -22,9 +23,9 @@ int poll(struct pollfd *fds, nfds_t nfds, int timeout) {
 		if (SET_POLL_C) {
 			SET_POLL = atoi(SET_POLL_C);
 		}
-    }
-    if (timeout < MIN_POLL) {
-        timeout = SET_POLL;
-    }
-    return poll_orig(fds, nfds, timeout);
+	}
+	if (timeout < MIN_POLL) {
+		timeout = SET_POLL;
+	}
+	return poll_orig(fds, nfds, timeout);
 }
